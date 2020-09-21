52735825bdea65a2514a834390d9a14b33a38b63
Knoah Cotto <cotto.knoah@gmail.com>
2020-09-20 23:32:56 -0500

    added hw constants

--- src/sample/colorControlPanel.java
+++ src/sample/colorControlPanel.java
@@ -1,4 +1,23 @@
 package sample;
 
-public class colorControlPanel {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class colorControlPanel extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        // Constants
+        final double MIN = 0.0, MAX = 255.0, INITIAL = 0.0;
+        final double MAJOR_TICK_UNIT = 15;
+        final int MINOR_TICK_COUNT = 5;
+        final double SLIDER_WIDTH = 512.0;
+        final double SPACING = 10.0;
+
+    }
 }
