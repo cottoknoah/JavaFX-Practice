3c070ceb7282d5da9b9d4787d4e392f218eeb0b4
Knoah Cotto <cotto.knoah@gmail.com>
2020-09-30 13:18:38 -0500

    figuring out Hbox

--- src/tutoring/circleGallery.java
+++ src/tutoring/circleGallery.java
@@ -1,7 +1,30 @@
 package tutoring;
 
-public class circleGallery {
-}
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.shape.Circle;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+
+    public class circleGallery extends Application {
+        @Override
+        public void start(Stage primaryStage) {
+            Circle circle = new Circle();
+            Label heightLabel = new Label("Height:");
+            TextField heightTextField = new TextField();
+            HBox heightHbox = new Hbox(5, heightLabel, heightTextField);
+            heightHbox.setAlignment(Pos.CENTER);
+            heightHbox.setPadding(new Insets(10,0,0,0));
+
+            Button makeCircle = new Button("Create the Circle");
+
+        }
+    }
+
 
 //        3. In CircleGallery, make sure you have the required imports for building a JavaFX
 //        application as well as the imports for a TextField, Label, Button, Hbox, and VBox. You
