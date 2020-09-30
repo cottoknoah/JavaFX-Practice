c7da567cc970889e73b28458584583f703c87308
Knoah Cotto <cotto.knoah@gmail.com>
2020-09-30 13:19:03 -0500

    Hbox still wont show

--- src/tutoring/circleGallery.java
+++ src/tutoring/circleGallery.java
@@ -3,10 +3,14 @@ package tutoring;
 import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 
@@ -21,10 +25,19 @@ import javafx.geometry.Insets;
             heightHbox.setPadding(new Insets(10,0,0,0));
 
             Button makeCircle = new Button("Create the Circle");
-
+//    circle.setCenterX(100);
+//    circle.setCenterY(100);
+//    circle.setRadius(50);
+//    circle.setStroke(Color.GREEN);
+//    circle.setFill(Color.RED);
+//
+//    Pane pane = new Pane();
+//    pane.getChildren() .add(circle);
+//    Scene scene = new Scene(pane , 200, 200);
+//    primaryStage.setTitle("ShowCircle");
+//    primaryStage.show();
         }
     }
-
 
 //        3. In CircleGallery, make sure you have the required imports for building a JavaFX
 //        application as well as the imports for a TextField, Label, Button, Hbox, and VBox. You
