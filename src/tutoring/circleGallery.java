9f7b400180c5c69e95863e502d8917fe711f1a99
Knoah Cotto <cotto.knoah@gmail.com>
2020-10-01 12:31:13 -0500

    checked imports

--- src/tutoring/circleGallery.java
+++ src/tutoring/circleGallery.java
@@ -4,9 +4,7 @@ import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -14,19 +12,6 @@ import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 
-import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
 
 public class circleGallery extends Application {
     @Override
