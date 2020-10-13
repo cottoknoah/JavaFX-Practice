9d5b7643293d7eabdf002428b45ae6222627355d
Knoah Cotto <cotto.knoah@gmail.com>
2020-10-12 22:09:20 -0500

    rereading instructions

--- src/tutoring/Inventory.java
+++ src/tutoring/Inventory.java
@@ -1,5 +1,22 @@
 package tutoring;
 
-public class Inventory {
-}
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import java.util.ArrayList;
 
+public class Inventory extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+}
