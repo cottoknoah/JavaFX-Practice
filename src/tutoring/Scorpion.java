aea0891ccf0e0ab5f557afb456b7b86f5ad9b94c
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-05 19:56:20 -0600

    almost done with added classes

---
+++ src/tutoring/Scorpion.java
@@ -0,0 +1,12 @@
+package tutoring;
+
+public class Scorpion extends Arachnid {
+
+    public Scorpion() {
+        super("Scorpion");
+    }
+
+    public String toString() {
+        return super.toString() + " They have been around for 300 million years.";
+    }
+}
