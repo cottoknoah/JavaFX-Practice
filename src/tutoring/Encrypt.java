bfaa96aff16247470d1e6686d97435152e3c4b2e
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-12 23:18:16 -0600

    added for loop

--- src/tutoring/Encrypt.java
+++ src/tutoring/Encrypt.java
@@ -5,5 +5,20 @@ public class Encrypt {
     static char xorKey = 'X';
     public Encrypt(){}
 
+    public static char[] blockEncrypt(char[] arr) {
+        int size = arr.length;
+
+        // round up size for blocks of 5
+        while(size % 5 != 0) {
+            size++;
+        }
+
+        char[] newArr = new char[size];
+        char[] tempArr = new char[5];
+        int pos = 0;
+        int loc = 0; // location of tempArr
+        for(int i = 0; i < size; i++) {
+            char val;
+
 
 }
