e538e367ee4bed8539841f0bf73f54f379732d87
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-12 23:16:27 -0600

    working on more cyphers

---
+++ src/tutoring/MainTwo.java
@@ -0,0 +1,25 @@
+package tutoring;
+
+
+public class MainTwo {
+
+    static int a = 3;
+    static int b = 5;
+    static char xorKey = 'X';
+
+    // my message1 HELLO = KHOOR
+    private static char[] message = {'H', 'E', 'L', 'L', 'O'};
+    // my message2 XYZA = ABCD
+    private static char[] message2 = {'X', 'Y', 'Z', 'A'};
+
+    // my message3 HOME
+    private static char[] message3 = {'H', 'O', 'M', 'E'};
+
+    // my message4 GRIZZLY BEARS
+    private static char[] message4 = {'G', 'R', 'I', 'Z', 'Z', 'L', 'Y', ' ', 'B', 'E', 'A', 'R', 'S'};
+
+    public static void main(String[] args) {
+
+    }
+
+}
