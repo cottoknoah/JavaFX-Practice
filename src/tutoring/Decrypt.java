61f25ef87a7f88f4693ab02f33fba43a193f2402
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-15 19:17:59 -0600

    working on logic

--- src/tutoring/Decrypt.java
+++ src/tutoring/Decrypt.java
@@ -10,8 +10,8 @@ public class Decrypt {
     public static char[] blockDecrypt(char[] arr) {
         int size = arr.length;
 
-        // round up size for blocks of 5
-        while (size % 5 != 0) {
+
+        while(size % 5 != 0) {
             size++;
         }
 
@@ -19,10 +19,10 @@ public class Decrypt {
         char[] tempArr = new char[5];
         int pos = 0;
         int loc = 0; // location of tempArr
-        for (int i = 0; i < size; i++) {
+        for(int i = 0; i < size; i++) {
             char val;
-            // Determine if need to fill with X
-            if (i >= arr.length) {
+
+            if(i >= arr.length) {
                 val = 'X';
             } else {
                 val = arr[i];
@@ -30,21 +30,44 @@ public class Decrypt {
             switch (pos) {
 
                 case 2:
-                    tempArr[0] = (char) ((int) val ^ xorKey);
+                    tempArr[0] = (char)((int) val ^ xorKey);
                     break;
                 case 4:
-                    tempArr[1] = (char) ((int) val ^ xorKey);
+                    tempArr[1] = (char)((int) val ^ xorKey);
                     break;
                 case 0:
-                    tempArr[2] = (char) ((int) val ^ xorKey);
+                    tempArr[2] = (char)((int) val ^ xorKey);
                     break;
                 case 1:
-                    tempArr[3] = (char) ((int) val ^ xorKey);
+                    tempArr[3] = (char)((int) val ^ xorKey);
                     break;
                 case 3:
-                    tempArr[4] = (char) ((int) val ^ xorKey);
+                    tempArr[4] = (char)((int) val ^ xorKey);
                     break;
+            }
+
+            if(pos == 4) {
+                pos = 0;
+
+                for(int x = 0; x < 5; x++) {
+                    newArr[loc+x] = tempArr[x];
+                }
+
+                loc += 5;
+            } else {
+                pos++;
             }
         }
 
+        for (int i = 0; i < newArr.length; i++) {
+            int endLen = newArr.length - 5;
+
+            if(i > endLen) {
+                if(newArr[i] == 'X') {
+                    newArr[i] = 0;
+                }
+            }
+        }
+        return newArr;
     }
+}
