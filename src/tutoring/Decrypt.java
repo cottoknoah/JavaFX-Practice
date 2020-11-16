2d0e036240995d1c0f5ce0cbc5527265a5b1ea01
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-15 19:17:18 -0600

    switch case

--- src/tutoring/Decrypt.java
+++ src/tutoring/Decrypt.java
@@ -10,10 +10,41 @@ public class Decrypt {
     public static char[] blockDecrypt(char[] arr) {
         int size = arr.length;
 
-
+        // round up size for blocks of 5
         while (size % 5 != 0) {
             size++;
         }
-    }
 
-}
+        char[] newArr = new char[size];
+        char[] tempArr = new char[5];
+        int pos = 0;
+        int loc = 0; // location of tempArr
+        for (int i = 0; i < size; i++) {
+            char val;
+            // Determine if need to fill with X
+            if (i >= arr.length) {
+                val = 'X';
+            } else {
+                val = arr[i];
+            }
+            switch (pos) {
+
+                case 2:
+                    tempArr[0] = (char) ((int) val ^ xorKey);
+                    break;
+                case 4:
+                    tempArr[1] = (char) ((int) val ^ xorKey);
+                    break;
+                case 0:
+                    tempArr[2] = (char) ((int) val ^ xorKey);
+                    break;
+                case 1:
+                    tempArr[3] = (char) ((int) val ^ xorKey);
+                    break;
+                case 3:
+                    tempArr[4] = (char) ((int) val ^ xorKey);
+                    break;
+            }
+        }
+
+    }
