448b5eafbbae1eba21b91ccdffab5743afafcfe7
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-15 19:16:44 -0600

    starting while loop

--- src/tutoring/Decrypt.java
+++ src/tutoring/Decrypt.java
@@ -1,4 +1,19 @@
 package tutoring;
 
+
 public class Decrypt {
+
+    static char xorKey = 'X';
+
+    public Decrypt(){}
+
+    public static char[] blockDecrypt(char[] arr) {
+        int size = arr.length;
+
+
+        while (size % 5 != 0) {
+            size++;
+        }
+    }
+
 }
