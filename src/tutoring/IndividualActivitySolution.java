f55a12eaacf646bd82784331e44a1a9a613b6822
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-17 21:20:17 -0600

    presentation for student

--- src/tutoring/IndividualActivitySolution.java
+++ src/tutoring/IndividualActivitySolution.java
@@ -25,6 +25,52 @@ public class IndividualActivitySolution
 
         System.out.println();
 
+        /***** AFFINE CIPHER *****/
+        // Using a = 3 and b = 5
+        for (int index = 0; index < message.length; index++)
+            message[index] = (char) (((message[index] - 65) * 3 + 5) % 26 + 65);
+
+        for (int index = 0; index < message.length; index++)
+        {
+            if ((message[index] - 65 - 5) % 3 == 0)
+                message[index] = (char) (((message[index] - 65 - 5) / 3 % 26 + 65));
+            else if ((message[index] - 65 - 5 + 26) % 3 == 0)
+                message[index] = (char) (((message[index] - 65 - 5 + 26) / 3 % 26 + 65));
+            else if ((message[index] - 65 - 5 + 52) % 3 == 0)
+                message[index] = (char) (((message[index] - 65 - 5 + 52) / 3 % 26 + 65));
+        }
+
+        for (char letter : message)
+            System.out.print(letter);
+
+        System.out.println();
+
+        /***** BLOCK CIPHER *****/
+        char[] paddedBears = new char[(int) Math.ceil(bears.length / 5.0) * 5];
+        char[] swappedBears = new char[(int) Math.ceil(bears.length / 5.0) * 5];
+
+        for (int index = 0; index < paddedBears.length; index++)
+        {
+            if (index < bears.length)
+                paddedBears[index] = bears[index];
+            else
+                paddedBears[index] = 'X';
+        }
+
+        for (int index = 0; index < paddedBears.length; index++)
+        {
+            if (index % 5 == 0)
+                swappedBears[index + 2] = paddedBears[index];
+            else if (index % 5 == 1)
+                swappedBears[index + 3] = paddedBears[index];
+            else if (index % 5 == 2)
+                swappedBears[index - 2] = paddedBears[index];
+            else if (index % 5 == 3)
+                swappedBears[index - 2] = paddedBears[index];
+            else if (index % 5 == 4)
+                swappedBears[index - 1] = paddedBears[index];
+        }
+
         for (char letter : swappedBears)
             System.out.print(letter);
 
