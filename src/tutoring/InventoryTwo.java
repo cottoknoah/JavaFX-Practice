e3b0beb1ccfe071f1a5931c67a43dead26f1e700
Knoah Cotto <cotto.knoah@gmail.com>
2020-11-28 18:01:05 -0600

    looping through logic

--- src/tutoring/InventoryTwo.java
+++ src/tutoring/InventoryTwo.java
@@ -84,5 +84,21 @@ public class InventoryTwo extends Application
         // One by one move boundary of unsorted subarray
         for (int i = 0; i < n-1; i++)
         {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr.get(j).compareItems(arr.get(min_idx)) < 0)
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            Item temp = arr.get(min_idx);
+            arr.set(min_idx, arr.get(i));
+            arr.set(i,temp);
+        }
+    }
+
+
+    // IMPLEMENT BINARY SEARCH METHOD
 
 }
